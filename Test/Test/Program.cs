e0caa76c8db698667a09a4dd94d6cb84213c08e3
Mikhail Sementsov <fewebdev@gmail.com>
2020-09-10 21:14:19 +0300

    reverse string

---
+++ Test/Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] testArray = { "", "tobor", "nemaR", "!yrgnuh m'I", "racecar", "reward" };
+
+            foreach (string item in testArray)
+            {
+                Console.WriteLine($"{item} {item == RevStr(RevStr(item))}");
+            }
+        }
+
+        static string RevStr(string input)
+        {
+            string temp = "";
+            for (int i = input.Length; i > 0; i--) temp += input[i - 1];
+            return temp;
+        }
+    }
+}
---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        return RevStr(input);
+    }
+
+    static string RevStr(string input)
+    {
+        string temp = "";
+        for (int i = input.Length; i > 0; i--) temp += input[i - 1];
+        return temp;
+    }
+
+    static string RevStr2(string input)
+    {
+        char[] charArr = input.ToCharArray();
+        Array.Reverse(charArr);
+        return String.Join("", charArr);
+    }
+}
