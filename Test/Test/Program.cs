2e85007fb211b460cc509eb2d272e6da84d5abb5
Mikhail Sementsov <fewebdev@gmail.com>
2020-09-12 22:13:21 +0300

    pangram

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -7,55 +7,22 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            // Assert.Equal("UGCACCAGAAUU", RnaTranscription.ToRna("ACGTGGTCTTAA"));
-            var testStr = "ACGTGGTCTTAA";
-            Console.WriteLine(ToRna3(testStr));
-            Console.WriteLine("UGCACCAGAAUU");
+            string test = "nopqrstuvwxyz";
+            string test2 = "The quick brown fox jumps over the lazy dog.";
+            Console.WriteLine(Pangram(test2));
         }
 
-        public static string ToRna(string nucleotide)
-        {
-            var (DNA, RNA) = ("GCTA", "CGAU");
-            string temp = "";
-            foreach (char item in nucleotide) temp += RNA[DNA.IndexOf(item)];
-            return temp;
-        }
 
-        public static string ToRna2(string nucleotide)
+
+        public static bool Pangram(string input)
         {
-            string temp = "";
-            foreach (char item in nucleotide)
+            var alphabet = "abcdefghijklmnopqrstuvwxyz";
+            foreach (char item in input.ToLower())
             {
-                switch (item)
-                {
-                    case 'G':
-                        temp += 'C';
-                        break;
-                    case 'C':
-                        temp += 'G';
-                        break;
-                    case 'T':
-                        temp += 'A';
-                        break;
-                    case 'A':
-                        temp += 'U';
-                        break;
-                    default:
-                        break;
-                }
+                if (alphabet.Contains(item)) alphabet = alphabet.Remove(alphabet.IndexOf(item), 1);
             }
-            return temp;
-        }
 
-        public static string ToRna3(string nucleotide)
-        {
-            Dictionary<char, char> nucleo = new Dictionary<char, char>
-        {
-            {'G','C'},{'C','G'},{'T','A'},{'A','U'}
-        };
-            string temp = "";
-            foreach (char item in nucleotide) temp += nucleo[item];
-            return temp;
+            return alphabet == "";
         }
     }
 }
---
+++ pangram/Pangram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        return Pang(input);
+    }
+
+    public static bool Pang(string input)
+    {
+        var alphabet = "abcdefghijklmnopqrstuvwxyz";
+        foreach (char item in input.ToLower())
+        {
+            if (alphabet.Contains(item)) alphabet = alphabet.Remove(alphabet.IndexOf(item), 1);
+        }
+
+        return alphabet == "";
+    }
+}
