26b3b403daaf212ecfabb6df7546dc740baa7437
Mikhail Sementsov <fewebdev@gmail.com>
2020-10-02 22:47:31 +0300

    main 15

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -15,28 +15,93 @@ namespace Test
             //var res = test.Select(PhoneNumber.Clean);
             //foreach (var item in res) Console.Write($"{item} ");
 
-            var a = PythagoreanTriplet.TripletsWithSum(1000);
-            //Console.WriteLine(10 / Math.Sqrt(2));
+            var cl = new Clock(0, 1441);
+            Console.WriteLine(cl.ToString());
         }
 
 
 
-        public static class PythagoreanTriplet
+
+
+        public class Clock
         {
-            public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
-            {
+            int hours = 0;
+            int minutes = 0;
 
-                for (var i = 0; i < sum; i++)
-                {
-                    var b = (sum * (sum - 2 * i)) / (2 * (sum - i));
-                    var c = sum - i - b;
-                    var cond1 = (i * i + b * b) == c * c;
-                    var cond2 = (i < b) && (b < c);
-                    if (cond1 && cond2) return new[] { 1,2,3};
-                }
-                return (2, 2, 3);
+
+
+            Clock UpdateTime(int extraHrs = 0, int extraMins = 0)
+            {
+                var temp = (minutes + extraMins + 60 * (hours + extraHrs)) % (24 * 60);
+                if (temp < 0) temp += 24 * 60;
+                hours = (temp / 60) % 24;
+                minutes = (temp - 60 * hours) % 60;
+                return this;
             }
+
+            public override string ToString()
+            {
+                var strHrs = $"{(hours < 10 ? "0" : "")}{hours}";
+                var strMins = $"{(minutes < 10 ? "0" : "")}{minutes}";
+                return strHrs + ":" + strMins;
+            }
+
+            public Clock(int hours, int minutes) => UpdateTime(hours, minutes);
+
+
+            public Clock Add(int minutesToAdd) => UpdateTime(extraMins: minutesToAdd);
+
+            public Clock Subtract(int minutesToSubtract) => UpdateTime(extraMins: -minutesToSubtract);
         }
+
+
+
+        //public void On_the_hour()
+        //{
+        //    var sut = new Clock(8, 0);
+        //    Assert.Equal("08:00", sut.ToString());
+        //}
+
+        //[Fact(Skip = "Remove this Skip property to run this test")]
+        //public void Past_the_hour()
+        //{
+        //    var sut = new Clock(11, 9);
+        //    Assert.Equal("11:09", sut.ToString());
+        //}
+
+        //[Fact(Skip = "Remove this Skip property to run this test")]
+        //public void Midnight_is_zero_hours()
+        //{
+        //    var sut = new Clock(24, 0);
+        //    Assert.Equal("00:00", sut.ToString());
+        //}
+
+        //[Fact(Skip = "Remove this Skip property to run this test")]
+        //public void Hour_rolls_over()
+        //{
+        //    var sut = new Clock(25, 0);
+        //    Assert.Equal("01:00", sut.ToString());
+        //}
+
+        //[Fact(Skip = "Remove this Skip property to run this test")]
+        //public void Hour_rolls_over_continuously()
+        //{
+        //    var sut = new Clock(100, 0);
+        //    Assert.Equal("04:00", sut.ToString());
+        //}
+
+        //[Fact(Skip = "Remove this Skip property to run this test")]
+        //public void Sixty_minutes_is_next_hour()
+        //{
+        //    var sut = new Clock(1, 60);
+        //    Assert.Equal("02:00", sut.ToString());
+        //}
+
+        //[Fact(Skip = "Remove this Skip property to run this test")]
+        //public void Minutes_roll_over()
+        //{
+        //    var sut = new Clock(0, 160);
+        //    Assert.Equal("02:40", sut.ToString());
 
     }
 }
---
+++ clock/Clock.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class Clock : IEquatable<Clock>
+{
+    int hours = 0,
+        minutes = 0;
+
+    (int, int) UpdateTime(int extraHrs = 0, int extraMins = 0)
+    {
+        var hrs = hours + extraHrs;
+        var mins = minutes + extraMins;
+        var temp = (mins + 60 * hrs) % (24 * 60);
+        if (temp < 0) temp += 24 * 60;
+        hrs = (temp / 60) % 24;
+        mins = (temp - 60 * hrs) % 60;
+        return (hrs, mins);
+    }
+
+    public Clock(int hours, int minutes) => (this.hours, this.minutes) = UpdateTime(hours, minutes);
+
+
+    public Clock Add(int minutesToAdd)
+    {
+        var (hrs, mins) = UpdateTime(extraMins: minutesToAdd);
+        return new Clock(hrs, mins);
+    }
+
+    public Clock Subtract(int minutesToSubtract)
+    {
+        var (hrs, mins) = UpdateTime(extraMins: -minutesToSubtract);
+        return new Clock(hrs, mins);
+    }
+
+    public bool Equals(Clock clock) => clock.ToString() == ToString();
+
+    public override string ToString()
+    {
+        var strHrs = $"{(hours < 10 ? "0" : "")}{hours}";
+        var strMins = $"{(minutes < 10 ? "0" : "")}{minutes}";
+        return $"{strHrs}:{strMins}";
+    }
+}
