be043a9f570f618ef280882cf686d9b5ffdd6e58
Mikhail Sementsov <fewebdev@gmail.com>
2020-10-09 23:48:24 +0300

    side 19

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -16,11 +16,11 @@ namespace Test
             //var array = new[] { 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634 };
             //var value = 144;
 
-            var array = new[] { 1, 3,21, 5, 8 };
+            var array = new[] { 1, 3, 21, 5, 8 };
             var value = 21;
 
-            var res = BinarySearch.Find(array, value);
-            Console.WriteLine(res);
+            //var res = BinarySearch.Find(array, value);
+            Console.WriteLine(BeerSong.G(99,2));
         }
 
     }
@@ -29,39 +29,57 @@ namespace Test
 
 
 
-    public static class BinarySearch
+
+
+    public static class BeerSong
     {
-        public static int Find(int[] input, int value)
+        public static string GetFragment(int num)
         {
-            int GetMiddleIdx() => (int)Math.Round((float)input.Length / 2);
+            var template1 = $"{num} bottles of beer on the wall, {num} bottles of beer.\n" +
+            $"Take one down and pass it around, {num - 1} bottles of beer on the wall.\n\n";
 
-            if (input.Length == 0) return -1;
+            var template2 = $"2 bottles of beer on the wall, 2 bottles of beer.\n" +
+            $"Take one down and pass it around, 1 bottle of beer on the wall.\n\n";
 
-            int idx, idxPre = -1, middleIdx;
-            idx = middleIdx = GetMiddleIdx();
+            var template3 = $"1 bottle of beer on the wall, 1 bottle of beer.\n" +
+             $"Take one down and pass it around, no more bottles of beer on the wall.\n\n";
 
-            while (input[middleIdx] != value)
+            var template4 = $"No more bottles of beer on the wall, no more bottles of beer.\n" +
+             $"Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
+
+            if (num == 2) return template2;
+            if (num == 1) return template3;
+            if (num == 0) return template4;
+            return template1;
+        }
+
+        public static string Recite(int startBottles, int takeDown)
+        {
+            var res = "";
+
+            for (var i = startBottles; i > startBottles-takeDown; i--)
             {
-                if (value > input[middleIdx])
-                {
-                    input = input[middleIdx..];
-                    middleIdx = GetMiddleIdx();
-                    idx += middleIdx;
-                }
-                else
-                {
-                    input = input[..middleIdx];
-                    middleIdx = GetMiddleIdx();
-                    idx -= middleIdx;
-                }
-
-                if (idx == idxPre) return -1;
-                idxPre = idx;
+                res += GetFragment(i);
             }
 
-            return idx;
+            return res.Trim()
+                ;
+        }
+
+        public static string G(int startBottles, int takeDown)
+        {
+            return Enumerable.Range(startBottles-takeDown+1,takeDown).Reverse()
+           .Aggregate("", (acc, cur) => acc + " " + cur).Trim();
         }
     }
+
+    //var expected =
+    //            "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+    //            "Take one down and pass it around, 98 bottles of beer on the wall.\n" +
+    //            "\n" +
+    //            "98 bottles of beer on the wall, 98 bottles of beer.\n" +
+    //            "Take one down and pass it around, 97 bottles of beer on the wall.";
+    //Assert.Equal(expected, BeerSong.Recite(99, 2));
 
 
 
---
+++ beer-song/BeerSong.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+public static class BeerSong
+{
+    static string GetPart(int num)
+    {
+        var template1 = $"{num} bottles of beer on the wall, {num} bottles of beer.\n" +
+        $"Take one down and pass it around, {num - 1} bottles of beer on the wall.\n\n";
+
+        var template2 = $"2 bottles of beer on the wall, 2 bottles of beer.\n" +
+        $"Take one down and pass it around, 1 bottle of beer on the wall.\n\n";
+
+        var template3 = $"1 bottle of beer on the wall, 1 bottle of beer.\n" +
+         $"Take it down and pass it around, no more bottles of beer on the wall.\n\n";
+
+        var template4 = $"No more bottles of beer on the wall, no more bottles of beer.\n" +
+         $"Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
+
+        if (num == 2) return template2;
+        if (num == 1) return template3;
+        return num == 0 ? template4 : template1;
+    }
+
+    public static string Recite(int startBottles, int takeDown) =>
+        Enumerable.Range(startBottles - takeDown + 1, takeDown).Reverse()
+           .Aggregate("", (acc, cur) => acc + GetPart(cur)).Trim();
+}
